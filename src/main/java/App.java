67a26b02724b965cc90a53e4ac446001e7ff3c07
Nelly Cheruiyot <nellycheruiyot@gmail.com>
2017-12-08 12:53:21 +0300

    Add gradle wrapper

---
+++ src/main/java/App.java
@@ -0,0 +1,77 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import static spark.Spark.*;
+
+public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    ProcessBuilder process = new ProcessBuilder();
+     Integer port;
+     if (process.environment().get("PORT") != null) {
+         port = Integer.parseInt(process.environment().get("PORT"));
+     } else {
+         port = 4567;
+     }
+
+    setPort(port);
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      // model.put("tasks", request.session().attribute("tasks"));
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("tasks/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/task-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/tasks", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("tasks", Task.all());
+      model.put("template", "templates/tasks.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/tasks/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Task task = Task.find(Integer.parseInt(request.params(":id")));
+      model.put("task", task);
+      model.put("template", "templates/task.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/tasks", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String description = request.queryParams("description");
+      Task newTask = new Task(description);
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    //   ArrayList<Task> tasks = request.session().attribute("tasks");
+    //   if (tasks == null) {
+    //     tasks = new ArrayList<Task>();
+    //     request.session().attribute("tasks", tasks);
+    //   }
+    //
+    //   String description = request.queryParams("description");
+    //   Task newTask = new Task(description);
+    //   tasks.add(newTask);
+    //   // request.session().attribute("task", newTask);
+    //
+    //   model.put("template", "templates/success.vtl");
+    //   return new ModelAndView(model, layout);
+    // }, new VelocityTemplateEngine());
+
+  }
+}
---
+++ src/test/java/CategoryTest.java
@@ -0,0 +1,46 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class CategoryTest {
+
+  @Test
+  public void category_instantiatesCorrectly_true() {
+    Category testCategory = new Category("Home");
+    assertEquals(true, testCategory instanceof Category);
+  }
+
+  @Test
+  public void getName_categoryInstantiatesWithName_Home() {
+    Category testCategory = new Category("Home");
+    assertEquals("Home", testCategory.getName());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfCategory_true() {
+    Category firstCategory = new Category("Home");
+    Category secondCategory = new Category("Work");
+    assertEquals(true, Category.all().contains(firstCategory));
+    assertEquals(true, Category.all().contains(secondCategory));
+  }
+
+  @Test
+  public void clear_emptiesAllCategoriesFromList_0() {
+    Category testCategory = new Category("Home");
+    Category.clear();
+    assertEquals(Category.all().size(), 0);
+  }
+
+  @Test
+  public void getId_categoriesInstantiatesWithAnId_1() {
+    Category testCategory = new Category("Home");
+    assertEquals(1, testCategory.getId());
+  }
+
+  @Test
+  public void find_returnsCategoryWithSameId_secondCategory() {
+    Category.clear();
+    Category firstCategory = new Category("Home");
+    Category secondCategory = new Category("Work");
+    assertEquals(Category.find(secondCategory.getId()), secondCategory);
+  }
+}
